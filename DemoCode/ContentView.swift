8d1166d82d64d14583351ca5eb20b50c985a1ffa
jujien <vukien104@icloud.com>
2019-10-12 15:22:51 +0700

    Initial Commit

---
+++ DemoCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoCode
+//
+//  Created by jujien on 10/12/19.
+//  Copyright © 2019 jujien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoCodeTests/DemoCodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  DemoCodeTests.swift
+//  DemoCodeTests
+//
+//  Created by jujien on 10/12/19.
+//  Copyright © 2019 jujien. All rights reserved.
+//
+
+import XCTest
+@testable import DemoCode
+
+class DemoCodeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
